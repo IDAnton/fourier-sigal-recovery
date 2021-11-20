624f1e04171ee8ac0911dc8112d572071d71eed6
W0rth-it <anton-qz@yandex.ru>
2021-11-20 13:37:56 +0700

    solved

--- ffilter.py
+++ ffilter.py
@@ -4,10 +4,10 @@ import matplotlib.figure as mpl_fig
 import matplotlib.animation as anim
 import random
 
-from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QApplication, QPushButton, \
-QLineEdit, QGridLayout, QLabel, QCheckBox, QVBoxLayout, QWidget, QSlider, QMessageBox
-from PyQt5.QtGui import QPixmap, QDoubleValidator, QPalette, QColor
-from PyQt5.QtCore import Qt, QLocale
+# from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QApplication, QPushButton, \
+# QLineEdit, QGridLayout, QLabel, QCheckBox, QVBoxLayout, QWidget, QSlider, QMessageBox
+# from PyQt5.QtGui import QPixmap, QDoubleValidator, QPalette, QColor
+# from PyQt5.QtCore import Qt, QLocale
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from math import pi
 from scipy.fft import rfft, irfft, rfftfreq
@@ -156,147 +156,147 @@ class FFPlotter(FigureCanvas, anim.FuncAnimation):
         self.redraw = state
     
 
-class Gui(QMainWindow):
-    def __init__(self, top=300, left=300, width=1100, height=700, ff:FourierFilter=None):
-        super().__init__()
-        self.ff = ff
-        self.validator = QDoubleValidator()
-        self.validator.setLocale(QLocale("en_US"))
-        layout = self.init_widgets(width, height)
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+# class Gui(QMainWindow):
+#     def __init__(self, top=300, left=300, width=1100, height=700, ff:FourierFilter=None):
+#         super().__init__()
+#         self.ff = ff
+#         self.validator = QDoubleValidator()
+#         self.validator.setLocale(QLocale("en_US"))
+#         layout = self.init_widgets(width, height)
+#         widget = QWidget()
+#         widget.setLayout(layout)
+#         self.setCentralWidget(widget)
 
-    def init_widgets(self, width, height):
-        layout = QHBoxLayout()
-        right_layout = QVBoxLayout()
-        grid = QGridLayout()
-        grid.setSpacing(15)
-        self.signal_widgets = []
-        for i, name in zip(range(3), ["Amplitude", "Frequency", "Phase pi "]):
-            lable = QLabel(self, text=name)
-            lable.setFixedHeight(22)
-            grid.addWidget(lable, 0, i+1)
-        for i in range(1, 4):
-            grid.addWidget(QLabel(f"Signal №{i}"), i, 0)
-            self.signal_widgets.append([])
-            for j in range(1, 3):
-                area = QLineEdit(str(self.ff.signals_params[i-1][j-1]), self)
-                grid.addWidget(area, i, j)
-                area.setValidator(self.validator)
-                area.textEdited.connect(self.update_signal_props)
-                self.signal_widgets[i-1].append(area)
-            phase_widget = QSlider(Qt.Horizontal, self)
-            phase_widget.setMaximum(100)
-            phase_widget.setSingleStep(1)
-            phase_widget.setValue(int(self.ff.signals_params[i-1][2] / np.pi * 100))
-            phase_widget.setMinimumSize(100, 10)
-            grid.addWidget(phase_widget, i, 3)
-            phase_widget.sliderMoved.connect(self.update_signal_props)
-            self.signal_widgets[i-1].append(phase_widget)
+#     def init_widgets(self, width, height):
+#         layout = QHBoxLayout()
+#         right_layout = QVBoxLayout()
+#         grid = QGridLayout()
+#         grid.setSpacing(15)
+#         self.signal_widgets = []
+#         for i, name in zip(range(3), ["Amplitude", "Frequency", "Phase pi "]):
+#             lable = QLabel(self, text=name)
+#             lable.setFixedHeight(22)
+#             grid.addWidget(lable, 0, i+1)
+#         for i in range(1, 4):
+#             grid.addWidget(QLabel(f"Signal №{i}"), i, 0)
+#             self.signal_widgets.append([])
+#             for j in range(1, 3):
+#                 area = QLineEdit(str(self.ff.signals_params[i-1][j-1]), self)
+#                 grid.addWidget(area, i, j)
+#                 area.setValidator(self.validator)
+#                 area.textEdited.connect(self.update_signal_props)
+#                 self.signal_widgets[i-1].append(area)
+#             phase_widget = QSlider(Qt.Horizontal, self)
+#             phase_widget.setMaximum(100)
+#             phase_widget.setSingleStep(1)
+#             phase_widget.setValue(int(self.ff.signals_params[i-1][2] / np.pi * 100))
+#             phase_widget.setMinimumSize(100, 10)
+#             grid.addWidget(phase_widget, i, 3)
+#             phase_widget.sliderMoved.connect(self.update_signal_props)
+#             self.signal_widgets[i-1].append(phase_widget)
 
-        grid.addWidget(QLabel("Noise amplitude"), 4, 0, 1, 2, alignment=Qt.AlignRight)
-        self.noise = QLineEdit(str(self.ff.noise_amp), self)
-        self.noise.setValidator(self.validator)
-        self.noise.textEdited.connect(self.update_signal_props)
-        grid.addWidget(self.noise, 4, 2, 1, 2, alignment=Qt.AlignLeft )
+#         grid.addWidget(QLabel("Noise amplitude"), 4, 0, 1, 2, alignment=Qt.AlignRight)
+#         self.noise = QLineEdit(str(self.ff.noise_amp), self)
+#         self.noise.setValidator(self.validator)
+#         self.noise.textEdited.connect(self.update_signal_props)
+#         grid.addWidget(self.noise, 4, 2, 1, 2, alignment=Qt.AlignLeft )
 
-        grid.addWidget(QLabel("Measurements number"), 5, 0, 1, 2, alignment=Qt.AlignRight)
-        self.measurements = QSlider(Qt.Horizontal, self)
-        self.measurements.setMinimum(1)
-        self.measurements.setMaximum(40)
-        self.measurements.setValue(int(self.ff.ndots/10))
-        self.measurements.setTickInterval(10)
-        self.measurements.setSingleStep(10)
-        self.measurements.setPageStep(10)
-        self.measurements.setTickPosition(QSlider.TicksBelow)
-        self.measurements.valueChanged.connect(self.update_signal_props)
-        grid.addWidget(self.measurements, 5, 2)
-        self.measurements_number = QLabel(str(self.ff.ndots))
-        grid.addWidget(self.measurements_number, 5, 3, alignment=Qt.AlignCenter)
+#         grid.addWidget(QLabel("Measurements number"), 5, 0, 1, 2, alignment=Qt.AlignRight)
+#         self.measurements = QSlider(Qt.Horizontal, self)
+#         self.measurements.setMinimum(1)
+#         self.measurements.setMaximum(40)
+#         self.measurements.setValue(int(self.ff.ndots/10))
+#         self.measurements.setTickInterval(10)
+#         self.measurements.setSingleStep(10)
+#         self.measurements.setPageStep(10)
+#         self.measurements.setTickPosition(QSlider.TicksBelow)
+#         self.measurements.valueChanged.connect(self.update_signal_props)
+#         grid.addWidget(self.measurements, 5, 2)
+#         self.measurements_number = QLabel(str(self.ff.ndots))
+#         grid.addWidget(self.measurements_number, 5, 3, alignment=Qt.AlignCenter)
 
-        random_button = QPushButton("Generate random signal", self)
-        random_button.clicked.connect(self.generate_random)
-        grid.addWidget(random_button, 6, 1, alignment=Qt.AlignCenter)
+#         random_button = QPushButton("Generate random signal", self)
+#         random_button.clicked.connect(self.generate_random)
+#         grid.addWidget(random_button, 6, 1, alignment=Qt.AlignCenter)
 
-        self.animation_button = QPushButton("Toggle animation", self)
-        self.animation_button.setCheckable(True)
-        self.animation_button.clicked.connect(self.toggle_animation)
-        grid.addWidget(self.animation_button, 6, 2, alignment=Qt.AlignCenter)
+#         self.animation_button = QPushButton("Toggle animation", self)
+#         self.animation_button.setCheckable(True)
+#         self.animation_button.clicked.connect(self.toggle_animation)
+#         grid.addWidget(self.animation_button, 6, 2, alignment=Qt.AlignCenter)
 
-        grid.addWidget(QLabel("Redraw ticks labels (slow)"), 7, 1)
-        self.check = QCheckBox(self)
-        self.check.clicked.connect(lambda state: self.plotter.set_redraw(state))
-        grid.addWidget(self.check, 7, 2)
+#         grid.addWidget(QLabel("Redraw ticks labels (slow)"), 7, 1)
+#         self.check = QCheckBox(self)
+#         self.check.clicked.connect(lambda state: self.plotter.set_redraw(state))
+#         grid.addWidget(self.check, 7, 2)
 
-        save_button = QPushButton("Save to file", self)
-        save_button.clicked.connect(self.save)
-        grid.addWidget(save_button, 8, 1, 1, 2)
+#         save_button = QPushButton("Save to file", self)
+#         save_button.clicked.connect(self.save)
+#         grid.addWidget(save_button, 8, 1, 1, 2)
  
-        self.plotter = FFPlotter(ff=self.ff)
-        self.plotter.setFixedHeight(height)
-        layout.addWidget(self.plotter, 2, Qt.AlignLeft)
-        right_layout.setSpacing(20)
+#         self.plotter = FFPlotter(ff=self.ff)
+#         self.plotter.setFixedHeight(height)
+#         layout.addWidget(self.plotter, 2, Qt.AlignLeft)
+#         right_layout.setSpacing(20)
 
-        right_layout.addLayout(grid)
-        right_layout.addWidget(QLabel(self))
-        layout.addLayout(right_layout)
-        return layout
+#         right_layout.addLayout(grid)
+#         right_layout.addWidget(QLabel(self))
+#         layout.addLayout(right_layout)
+#         return layout
 
-    def toggle_animation(self):
-        if self.animation_button.isChecked():
-            self.animation_button.setStyleSheet("background-color : lightblue")
-            self.plotter.active = True
-        else:
-            self.plotter.active = False
-            self.animation_button.setStyleSheet("background-color : lightgrey")
+#     def toggle_animation(self):
+#         if self.animation_button.isChecked():
+#             self.animation_button.setStyleSheet("background-color : lightblue")
+#             self.plotter.active = True
+#         else:
+#             self.plotter.active = False
+#             self.animation_button.setStyleSheet("background-color : lightgrey")
 
-    def update_signal_props(self):
-        new_signal = []
-        try:
-            for i in range(3):
-                new_signal.append([])
-                for j in range (2):
-                    new_signal[i].append(float(self.signal_widgets[i][j].text()))
-                new_signal[i].append(float(self.signal_widgets[i][2].value()*np.pi/100))
-            noise = float(self.noise.text())
-            measurements = int(self.measurements.value()) * 10
-            self.measurements_number.setText(str(measurements))
-        except ValueError:
-            return
-        self.ff.signals_params = new_signal
-        self.ff.noise_amp = noise
-        self.ff.ndots = measurements
-        self.ff.init_signal()
-        self.ff.filter_signal()
-        self.plotter.need_update = True
+#     def update_signal_props(self):
+#         new_signal = []
+#         try:
+#             for i in range(3):
+#                 new_signal.append([])
+#                 for j in range (2):
+#                     new_signal[i].append(float(self.signal_widgets[i][j].text()))
+#                 new_signal[i].append(float(self.signal_widgets[i][2].value()*np.pi/100))
+#             noise = float(self.noise.text())
+#             measurements = int(self.measurements.value()) * 10
+#             self.measurements_number.setText(str(measurements))
+#         except ValueError:
+#             return
+#         self.ff.signals_params = new_signal
+#         self.ff.noise_amp = noise
+#         self.ff.ndots = measurements
+#         self.ff.init_signal()
+#         self.ff.filter_signal()
+#         self.plotter.need_update = True
 
-    def generate_random(self):
-        self.ff.random()
-        for i in range(3):
-                for j in range (2):
-                    self.signal_widgets[i][j].setText(str(self.ff.signals_params[i][j]))
-                self.signal_widgets[i][2].setValue(int(self.ff.signals_params[i][2]/np.pi*100))
-        self.noise.setText(str(self.ff.noise_amp))
-        self.update_signal_props()
+#     def generate_random(self):
+#         self.ff.random()
+#         for i in range(3):
+#                 for j in range (2):
+#                     self.signal_widgets[i][j].setText(str(self.ff.signals_params[i][j]))
+#                 self.signal_widgets[i][2].setValue(int(self.ff.signals_params[i][2]/np.pi*100))
+#         self.noise.setText(str(self.ff.noise_amp))
+#         self.update_signal_props()
 
-    def save(self):
-        pix = QPixmap(self.size())
-        self.render(pix)
-        pix.save('ffilter.png', 'png')
-        QMessageBox.about(self, "Saved", "Saved to file 'ffilter.png'")
-
-
-def main():
-    ff = FourierFilter(200)
-    ff.filter_signal()
-    app = QApplication(sys.argv)
-    app.setStyle('Fusion')
-    app.setStyleSheet("QLabel{font-size: 13pt;} QLineEdit{font-size: 13pt;} QPushButton{font-size: 13pt;}")
-    window = Gui(ff=ff)
-    window.show()
-    app.exec()
+#     def save(self):
+#         pix = QPixmap(self.size())
+#         self.render(pix)
+#         pix.save('ffilter.png', 'png')
+#         QMessageBox.about(self, "Saved", "Saved to file 'ffilter.png'")
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     ff = FourierFilter(200)
+#     ff.filter_signal()
+#     app = QApplication(sys.argv)
+#     app.setStyle('Fusion')
+#     app.setStyleSheet("QLabel{font-size: 13pt;} QLineEdit{font-size: 13pt;} QPushButton{font-size: 13pt;}")
+#     window = Gui(ff=ff)
+#     window.show()
+#     app.exec()
+
+
+# if __name__ == '__main__':
+#     main()
